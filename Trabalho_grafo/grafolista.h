a84258a9b79fdd3003c11a6652a7f7f4ecd5abe3
jesonwenchen <jesonwen2268@gmail.com>
2024-11-25 15:40:49 -0300

    Update grafolista.h

--- Trabalho_grafo/grafolista.h
+++ Trabalho_grafo/grafolista.h
@@ -33,4 +33,15 @@ Node** Lista_Sem_Peso(FILE* grafo, int* n, FILE* qvertice);
 
 void Graus_Lista(FILE* qvertice, int n, Node* listaadj[]);
 
+void criando_lista_peso(Node** adjList, int a, int b, float peso) 
+
+Node** lista_peso(FILE*grafo, int* n, FILE* qvertice)
+
+void Dijkstra_Lista(Node* lista[], int vertx, int n, FILE*qvertice)
+
+void Dijkstra_Matriz_Heap(float** matriz, int vertx, int n, FILE* qvertice)
+
+
+
+
 #endif
